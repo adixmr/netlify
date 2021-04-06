edea9706bb2083d5f19bc54cbab6608c816a1bac
Aditya <aditya.icf@gmail.com>
2021-04-06 18:23:16 +0530

    aca

---
+++ src/wp-content.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const fs = require("fs");
+const serverless = require('serverless-http')
+
+const router = express.Router()
+ 
+
+router.get('/:url(*)', async(req, res)=>{
+	res.redirect('https://defused.com/wp-content/'+req.params.url);	
+})
+
+
+
+app.use('/wp-content', router)
+
+
+module.exports.handler = serverless(app)
+
