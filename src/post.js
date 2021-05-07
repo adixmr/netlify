ea01ec2f64dec676a721599ca431e5e871cbf343
Aditya <aditya.icf@gmail.com>
2021-05-07 23:11:39 +0530

    17a

--- src/post.js
+++ src/post.js
@@ -13,15 +13,13 @@ router.get('/:id', async(req, res)=>{
     if(String(req.get('Referrer')).includes('facebook.com')){
         const data = await axios.get('https://defused.com/wp-json/wp/v2/posts/'+req.params.id)
         
+        if(req.query.utm!='' || req.query.utm!=undefined){
+         var query = '?utm_source='+req.query.utm+'&utm_medium='+req.query.utm+'&utm_campaign='+req.query.utm;
+        } else {
+         var query = '';
+        }
         
-              var query = [];
-         
-          for(key of Object.keys(req.query)){
-	  	query.push(key+'='+req.query[key]);
-	  }
-         
-	res.redirect(data.data.link + '?' + query.join('&')); 
-    
+	res.redirect(data.data.link + query); 
     	
     } else {
 
